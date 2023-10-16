92984789ddee0db72f9c933145e8f59ecb7f4382
Eleven <danielulelu@gmail.com>
2023-10-16 15:19:06 +0300

    Added showToast event on the Logout button in the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,12 @@
-import { Flex, Box, Heading, Text, Button, Spacer, HStack } from "@chakra-ui/react";
+import { Flex, Box, Heading, Text, Button, Spacer, HStack, useToast } from "@chakra-ui/react";
+import { UnlockIcon } from "@chakra-ui/icons";
 
 export default function Navbar() {
+  const toast = useToast();
+
+  const showToast = () => {
+    toast({ title: "Logged out", description: "Successfully logged out", duration: 3000, isClosable: true, status: "success", position: 'top', icon: <UnlockIcon />})
+  }
   return (
     <Flex as="nav" p="10px" mb="40px" alignItems="center" gap='10px'>
       <Heading as="h1">My Tasks</Heading>
@@ -11,7 +17,7 @@ export default function Navbar() {
             D
         </Box>
         <Text>danielulelu@gmail.com</Text>
-        <Button colorScheme='purple'>Logout</Button>
+        <Button colorScheme='purple' onClick={showToast}>Logout</Button>
       </HStack>
     </Flex>
   );

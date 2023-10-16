15e1744ba7cd1e990779a8922aaf1011626913e7
Eleven <danielulelu@gmail.com>
2023-10-16 09:00:44 +0300

    Added responsive style to the app and made use of `as` functionality to appropriately position the respective components

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,13 @@
-import { SimpleGrid, Box } from "@chakra-ui/react";
+import { SimpleGrid, Box, Text } from "@chakra-ui/react";
 
 export default function Dashboard() {
   
 
   return (
     <SimpleGrid columns={4} p="10px" spacing={10} minChildWidth="250px">
-      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid">
+        <Text color={{ base:  'blue', md: 'red', lg: 'green'}}>Hello to Luwis</Text>
+      </Box>
       <Box bg="white" h="200px" border="1px solid"></Box>
       <Box bg="white" h="200px" border="1px solid"></Box>
       <Box bg="white" h="200px" border="1px solid"></Box>

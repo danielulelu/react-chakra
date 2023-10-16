60c5a2ad1637937f63ee01382238d2a865c95c08
Eleven <danielulelu@gmail.com>
2023-10-16 15:10:02 +0300

    Added form fields and functionalities in the New Task component

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,15 @@ import {
 // layouts and a=pages
 import RootLayout from "./layouts/RootLayout";
 import Dashboard, { tasksLoader } from "./pages/Dashboard";
-import Create from "./pages/Create";
+import Create, { createTask } from "./pages/Create";
 import Profile from "./pages/Profile";
 import { ChakraProvider } from '@chakra-ui/react';
 
 // router and routes
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={<RootLayout />}>
-    <Route index element={<Dashboard />} loader={tasksLoader}/>
-    <Route path="create" element={<Create />}/>
+    <Route index element={<Dashboard />} loader={tasksLoader} />
+    <Route path="create" element={<Create />} action={createTask} />
     <Route path="profile" element={<Profile />}/>
     </Route>
 
--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,5 +1,54 @@
-export default function Create () {
+import {
+  Box,
+  Input,
+  FormLabel,
+  FormHelperText,
+  FormControl,
+  Textarea,
+  Checkbox,
+  Button
+} from "@chakra-ui/react";
+import { Form, redirect } from "react-router-dom";
+
+export default function Create() {
   return (
-    <div>Create</div>
-  )
+    <Box maxW="480px">
+      <Form method="post" action="/create">
+        <FormControl isRequired mb="40px">
+          <FormLabel>Task name:</FormLabel>
+          <Input type="text" name="title" />
+          <FormHelperText>Enter a descriptive task name</FormHelperText>
+        </FormControl>
+
+        <FormControl>
+          <FormLabel>Task description:</FormLabel>
+          <Textarea
+            placeholder="Enter a detailed description for the task"
+            name="description"
+          />
+        </FormControl>
+
+        <FormControl display="flex" alignItems="centre" mt="20px">
+          <Checkbox name="isPriority" size="lg" colorScheme="purple"/>
+          <FormLabel ml="15px">Make this a priority task</FormLabel>
+        </FormControl>
+
+        <Button type="submit">Submit</Button>
+      </Form>
+    </Box>
+  );
+}
+
+export const createTask = async ({ request }) => {
+  const data = await request.formData()
+
+  const task = {
+    title: data.get('title'),
+    description: data.get('description'),
+    isPriority: data.get('isPriority') === 'true' ? true : false
+  }
+
+  console.log(task)
+
+  return redirect('/')
 }
